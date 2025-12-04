2cb7c7d1d9f87903311b8e04b61756989bbc2cac
Carlos Diaz <carlosprimees@gmail.com>
2025-12-04 13:22:37 -0600

    Corrigiendo errores de configuracion para build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // 👈 AGREGAR ESTA LÍNEA ES ESENCIAL
+      base: "/UPP-IAP/", 
       server: {
         port: 3000,
         host: '0.0.0.0',
